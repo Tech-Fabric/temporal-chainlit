90b0e2a1d99d277f3322ac5ac764ce4c10bdd942
Sergey Ustimenko <sergey.ustimenko@techfabric.com>
2024-09-01 22:55:12 +0300

    Minor infrastructure improvements

--- temporal_worker.py
+++ temporal_worker.py
@@ -18,30 +18,35 @@ openai_gateway_url = os.getenv("OPENAI_GATEWAY_URL")
 TEMPORAL_HOST = os.getenv("TEMPORAL_HOST")
 TEMPORAL_PORT = os.getenv("TEMPORAL_PORT")
 
-
-print(TEMPORAL_HOST)
+temporal_connection_string = TEMPORAL_HOST + ":" + TEMPORAL_PORT
+print(temporal_connection_string)
 
 async def main():
-    client = await Client.connect(TEMPORAL_HOST + ":" + TEMPORAL_PORT)
+    while(True):
+        try:
+            client = await Client.connect(temporal_connection_string)
 
-    conversation_thread_activities = ConversationThreadActivities(openai_api_key, openai_gateway_url, openai_assistant_id)
+            conversation_thread_activities = ConversationThreadActivities(openai_api_key, openai_gateway_url, openai_assistant_id)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as activity_executor:
-        worker = Worker(
-            client,
-            task_queue="conversation-workflow-task-queue",
-            workflows=[ConversationThreadWorkflow],
-            activities=[
-                conversation_thread_activities.create_thread,
-                conversation_thread_activities.add_message_to_thread,
-                conversation_thread_activities.get_response,
-                conversation_thread_activities.function_call,
-                conversation_thread_activities.get_city,
-                conversation_thread_activities.submit_tool_call_result,
-            ],
-            activity_executor=activity_executor,
-        )
-        await worker.run()
+            with concurrent.futures.ThreadPoolExecutor(max_workers=100) as activity_executor:
+                worker = Worker(
+                    client,
+                    task_queue="conversation-workflow-task-queue",
+                    workflows=[ConversationThreadWorkflow],
+                    activities=[
+                        conversation_thread_activities.create_thread,
+                        conversation_thread_activities.add_message_to_thread,
+                        conversation_thread_activities.get_response,
+                        conversation_thread_activities.function_call,
+                        conversation_thread_activities.get_city,
+                        conversation_thread_activities.submit_tool_call_result,
+                    ],
+                    activity_executor=activity_executor,
+                )
+                await worker.run()
+        except RuntimeError as e:
+            print(e)
+            await asyncio.sleep(5)
 
 
 if __name__ == "__main__":
